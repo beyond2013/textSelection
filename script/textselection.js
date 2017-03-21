09bf8cc7e0a25fca26cd11bb5c6c253af69c9b0b
Imran Ali <imran.shahjee@gmail.com>
2017-03-21 11:59:12 +0700

    ignore tracking png files

--- script/textselection.js
+++ script/textselection.js
@@ -1,12 +1,17 @@
-getSelectionText = function(){
-  sentence = document.getElementById("sentence");
-  target= document.getElementById("target");
-  selection = window.getSelection();
-  range = document.createRange();
-  index = sentence.innerHTML.indexOf(target.value);
-  range.setStart(sentence.firstChild,index);
-  range.setEnd(sentence.firstChild,(index + target.value.length));
+getSource = function(){
+  var textNode = document.getElementById('sentence').firstChild,
+      source = document.getElementById('source');
+  var selection = document.getSelection(),
+      range  = document.createRange();
+  var value = source.value,
+      start = textNode.nodeValue.indexOf(value);
   selection.removeAllRanges();
+  if(value.length === 0 || start.index === -1){
+    return;
+  }
+
+  range.setStart(textNode,start);
+  range.setEnd(textNode,(start + value.length));
   selection.addRange(range);
 }
 
@@ -15,55 +20,46 @@ removeSelection = function(){
   selection.removeAllRanges();
 }
 
-extendSelection = function(buttonId){
-  var element = document.getElementById(buttonId);
-  if(element.id == "Previous"){
+extendSelectionForward = function(){
     var selection = window.getSelection();
-    selection.modify("extend", "left", "word");
-  }
-  else if(element.id == "Next"){
+    selection.modify('extend','right', 'word');
+}
+undoExtension = function(){
     var selection = window.getSelection();
-    selection.modify("extend","right", "word");
-
-  }
-  else if(element.id == "Move"){
-    var selection = window.getSelection();
-    console.log("selection.toString(): " + selection.toString());
-    console.log("selection.length: " + selection.toString().length);
-  range = document.createRange();
-  target = " " + previousWord() + " ";
-  indexPW = sentence.innerHTML.indexOf(target);
-  console.log("indexPW: " + indexPW);
-  range.setStart(sentence.firstChild,indexPW+1);
-  range.setEnd(sentence.firstChild,(index + selection.toString().length));
+    selection.modify('extend', 'left', 'word');
+}
+moveSelection = function(){
+  var textNode = document.getElementById('sentence').firstChild,
+      selection = window.getSelection(),
+      textNode = document.getElementById('sentence').firstChild,
+      range = document.createRange();
+  console.log('selection.toString(): ' + selection.toString());
+  console.log('selection.length: ' + selection.toString().length);
+  var target = ' ' + previousWord() + ' ';
+  indexPW = textNode.nodeValue.indexOf(target);
+  console.log('indexPW: ' + indexPW);
+  range.setStart(textNode,indexPW+1);
+  range.setEnd(textNode,(index + selection.toString().length));
   selection.removeAllRanges();
   selection.addRange(range);
-  }
 }
 
-tokenize = function(){
-var divsentence = document.getElementById("sentence");
-var sentence = divsentence.innerHTML;
-console.log(sentence);
-var words = sentence.split(" ");
-console.log(words);
-
-}
 
 previousWord = function(){
-  var pw="";
-  var sentence = document.getElementById("sentence").innerHTML;
-  var selection = window.getSelection();
-  console.log("selection: " + selection);
-  index = sentence.indexOf(selection);
-  console.log("indexOf(selection): " + index);
-  var sentArray = sentence.split(" ");
-  console.log("sentArray: " + sentArray);
-  var firstWord = selection.toString().split(" ")[0];
-  console.log("firstWord: " + firstWord);
+  var pw='',
+      textNode = document.getElementById('sentence').firstChild,
+      selection = window.getSelection();
+  console.log('textNode: ' + textNode);
+  console.log('selection: ' + selection);
+  index = textNode.nodeValue.indexOf(selection);
+  console.log('indexOf(selection): ' + index);
+  var sentArray = textNode.nodeValue.split(' ');
+  console.log('sentArray: ' + sentArray);
+  var firstWord = selection.toString().split(' ')[0];
+  console.log('firstWord: ' + firstWord);
   var firstWordIndex= sentArray.indexOf(firstWord);
   if(firstWordIndex >=1){
-    console.log("Previous Word :" + sentArray[firstWordIndex-1]);
+    console.log('Previous Word :' + sentArray[firstWordIndex-1]);
     pw = sentArray[firstWordIndex - 1];
   }
  else{
