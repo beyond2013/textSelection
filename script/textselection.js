91d356a2ddaf6d7e9aacae27ba129c8e6a9131ce
Imran Ali <imran.shahjee@gmail.com>
2017-04-24 23:51:27 +0700

    add function getText

--- script/textselection.js
+++ script/textselection.js
@@ -1,11 +1,53 @@
 window.onload = function(){
+  document.getElementById('addMarkup').addEventListener('click', addMarkup);
   document.getElementById('select').addEventListener('click', getSource);
+  document.getElementById('getText').addEventListener('click', getText);
   document.getElementById('undoExtension').addEventListener('click', undoExtension);
   document.getElementById('removeSelection').addEventListener('click', removeSelection);
   document.getElementById('extendForward').addEventListener('click', extendForward);
   document.getElementById('extendBackward').addEventListener('click', extendBackward);
 }
-getSource = function(){
+function addMarkup(){
+  var selection = window.getSelection().toString();
+  console.log(selection);
+  var markup= "<mark>".concat(selection).concat("</mark>");
+  var sent = document.getElementById('sentence').innerHTML;
+  document.getElementById("sentence").innerHTML=sent.replace(selection, markup);
+
+}
+function getText(){
+  var text = document.getElementById('sentence');
+  console.log("text: " + text);
+  source = document.getElementById('source');
+  var selection = document.getSelection(),
+      range  = document.createRange();
+  var value = source.value,
+      start = text.innerHTML.indexOf(value);
+  console.log("start= " + start);
+  selection.removeAllRanges();
+  if(value.length === 0 || start.index === -1){
+    return;
+  }
+  console.log("childNodes: " + text.childNodes.length);
+ var str="source located at: ";
+ if(text.hasChildNodes()){
+   for(var i=0;i< text.childNodes.length;i++)
+   {
+     childNode = text.childNodes[i];
+     if(childNode.nodeType == 3){
+      if(childNode.nodeValue.indexOf(value) >=0){
+         str += childNode.nodeValue; 
+         start = childNode.nodeValue.indexOf(value);
+         range.setStart(childNode,start);
+         range.setEnd(childNode,(start + value.length));
+         selection.addRange(range);
+      }
+     }
+   }
+ }
+
+}
+function getSource(){
   var textNode = document.getElementById('sentence').firstChild,
       source = document.getElementById('source');
   var selection = document.getSelection(),
@@ -16,26 +58,27 @@ getSource = function(){
   if(value.length === 0 || start.index === -1){
     return;
   }
-
   range.setStart(textNode,start);
   range.setEnd(textNode,(start + value.length));
   selection.addRange(range);
+
+
 }
 
-removeSelection = function(){
+function removeSelection(){
   selection = window.getSelection();
   selection.removeAllRanges();
 }
 
-extendForward = function(){
-    var selection = window.getSelection();
-    selection.modify('extend','right', 'word');
+function extendForward(){
+  var selection = window.getSelection();
+  selection.modify('extend','right', 'word');
 }
-undoExtension = function(){
-    var selection = window.getSelection();
-    selection.modify('extend', 'left', 'word');
+function undoExtension(){
+  var selection = window.getSelection();
+  selection.modify('extend', 'left', 'word');
 }
-extendBackward = function(){
+function extendBackward(){
   var textNode = document.getElementById('sentence').firstChild,
       selection = window.getSelection(),
       range = document.createRange();
@@ -54,7 +97,7 @@ extendBackward = function(){
 }
 
 
-previousWord = function(){
+function previousWord(){
   var pw='',
       textNode = document.getElementById('sentence').firstChild,
       selection = window.getSelection();
@@ -71,8 +114,8 @@ previousWord = function(){
     console.log('Previous Word :' + sentArray[firstWordIndex-1]);
     pw = sentArray[firstWordIndex - 1];
   }
- else{
-   pw = sentArray[0];
- }
- return pw;
+  else{
+    pw = sentArray[0];
+  }
+  return pw;
 } 
